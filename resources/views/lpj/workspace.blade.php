45f55f456569e1475af6ebcaf56b747f0397e7f7
Deni Ad <zainalmushaffa@gmail.com>
2026-02-22 07:15:15 +0700

    keterangan tidak ada nota

--- resources/views/lpj/workspace.blade.php
+++ resources/views/lpj/workspace.blade.php
@@ -1,8 +1,4 @@
 <x-app-layout>
-    
-    <style>
-        .scale-x-[-1] { transform: scaleX(-1); }
-    </style>
 
     {{-- WRAPPER UTAMA --}}
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-8 px-4">
@@ -336,9 +332,8 @@
         </svg>
     </a>
 @else
-    <span class="text-slate-300">
-        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-    </span>
+    <span class="p-4 text-sm font-medium text-red-400">Belum Ada
+           </span>
 @endif
 
         <td class="p-4 text-right font-mono text-sm tracking-tight">
